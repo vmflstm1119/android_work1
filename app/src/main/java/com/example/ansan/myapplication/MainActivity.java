f9ad53affd617c599629d0cea9ecdf3239822975
vmflstm1119 <vmflstm1119@naver.com>
2016-11-01 15:12:15 +0900

    Creat Android Project

--- app/src/main/java/com/example/ansan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ansan/myapplication/MainActivity.java
@@ -1,7 +1,21 @@
 package com.example.ansan.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +23,91 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        if (isStoragePermissionGranted() == false) {
+
+            Toast.makeText(this, "SD card 사용에러", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String strPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        //폴더생성
+
+        if(v.getId() == R.id.button4){
+            File mydir = new File(strPath + "/hongGiDong");
+            mydir.mkdir();
+
+            Toast.makeText(this, "폴더생성완료", Toast.LENGTH_SHORT).show();
+        }
+        else if(v.getId() == R.id.button5){
+            File mydir = new File(strPath + "/hongGiDong");
+            mydir.delete();
+            Toast.makeText(this, "폴더삭제완료", Toast.LENGTH_SHORT).show();
+        }
+
+        else if (v.getId() == R.id.button6) {
+            String filename = strPath + "/hongGiDong/test.txt";
+            try {
+                FileOutputStream fos = new FileOutputStream(filename);
+
+                fos.write("안녕하세요".getBytes());
+                fos.close();
+                Toast.makeText(this, "저장 완료", Toast.LENGTH_SHORT).show();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else if (v.getId() == R.id.button7) {
+            String filename = strPath + "/hongGiDong/test.txt";
+            FileInputStream fos = null;
+            try {
+                fos = new FileInputStream(filename);
+                byte arr[] = new byte[fos.available()];
+                fos.read();
+                fos.close();
+
+                String str = new String(arr);
+                Toast.makeText(this, "파일내용", Toast.LENGTH_SHORT).show();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+
+    String TAG = "[SDCAD]";
+
+    public boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG, "Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG, "Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG, "Permission is granted");
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.v(TAG, "Permission: " + permissions[0] + "was " + grantResults[0]);
+            //resume tasks needing this permission
+        }
     }
 }
